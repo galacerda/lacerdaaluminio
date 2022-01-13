b6804717675a3b2e1aad474136358b07a7cadb1b
galacerda <galacerda260401@gmail.com>
2022-01-13 20:17:58 -0300

    add: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,27 @@
 import Head from "next/head";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 import GlobalStyles from "../styles/globalStyles";
+import * as gtag from "../../public/lib/gtag";
 import AOS from "aos";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   useEffect(() => {
     AOS.init({});
   }, []);
+
   return (
     <>
       <Head>
